67cc514a5c3b87c8e54e00a7012edbeb7778466d
shaina <Shainagupta9253@gmail.com>
2022-05-23 11:10:38 -0700

    Exercise2 = list,tuple and set

---
+++ com/udemy/list,tuple,set/l,t,s.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    my_list = [25,50,25]
+    print(sum(my_list))
+
+    my_tuple = 12,
+    print(my_tuple)
+
+    set1 = {12,5,4,6,18,19}
+    set2 = {12,5,4,6}
+    result = set1.intersection(set2)
+    print(result)
