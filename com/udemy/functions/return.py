eac19385b01a004dd02c672a29d8d61f0ee7f023
shaina <Shainagupta9253@gmail.com>
2022-05-23 16:01:02 -0700

    Exercise4 = return function

---
+++ com/udemy/functions/return.py
@@ -0,0 +1,11 @@
+# Complete the function by making sure it returns 42. .
+
+if __name__ == '__main__':
+
+    def return_42():
+        return 42
+
+
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+    def my_function(arg1, arg2):
+        return arg1*arg2
