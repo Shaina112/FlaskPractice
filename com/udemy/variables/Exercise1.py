7fa9a9c29fa846d971d7c80195838892e022ce71
shaina <Shainagupta9253@gmail.com>
2022-05-23 00:24:17 -0700

    Added a project for Udemy Flask course and first exercise

---
+++ com/udemy/variables/Exercise1.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    # variables assignment
+    var1 = 12
+    var2 = var1
+    print(var2)
+
+    #multiplied the variables
+    num1 = 8
+    num2 = 2
+    print(num1 * num2)
