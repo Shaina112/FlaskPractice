9f09b71f34bc36f237c45d2ca524a398c412baef
shaina <Shainagupta9253@gmail.com>
2022-05-24 12:02:58 -0700

    Exercise1 - Variable excercise

--- com/udemy/variables/Exercise1.py
+++ com/udemy/variables/Exercise1.py
@@ -1,12 +1,6 @@
 
 
 if __name__ == '__main__':
-    # variables assignment
-    var1 = 12
-    var2 = var1
-    print(var2)
-
-    #multiplied the variables
-    num1 = 8
-    num2 = 2
-    print(num1 * num2)
+    User_age = int(input("Enter your age : "))
+    years = User_age * 3.154e+7
+    print(f" your age, {User_age}, is equal to {years} in seconds")
